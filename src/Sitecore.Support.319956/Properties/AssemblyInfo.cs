135e3005765e6da3f067cdcc0d9e48b5aa2240c8
Sergey Plashenko <sergey.plashenko@sitecore.com>
2019-12-06 15:56:59 +0200

    Initial commit

---
+++ src/Sitecore.Support.319956/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.319956")]
+[assembly: AssemblyProduct("Sitecore.Support.319956")]
+[assembly: ComVisible(false)]
